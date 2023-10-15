172d68c8bca00504155e1ab114ba37db36459a4c
UDMODZ <125041148+UDMODZ@users.noreply.github.com>
2023-10-15 13:08:30 +0530

    BY UDMODZ

---
+++ TMOD.sh
@@ -0,0 +1,77 @@
+#color
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+
+
+
+
+echo
+echo -e $b "┏┓─┏┓┏━━━┓┏━┓┏━┓┏━━━┓┏━━━┓┏━━━━┓ "
+echo -e $g "┃┃─┃┃┗┓┏┓┃┃┃┗┛┃┃┃┏━┓┃┗┓┏┓┃┗━━┓━┃ "
+echo -e $r  "┃┃─┃┃─┃┃┃┃┃┏┓┏┓┃┃┃─┃┃─┃┃┃┃──┏┛┏┛ "
+echo -e $b  "┃┃─┃┃─┃┃┃┃┃┃┃┃┃┃┃┃─┃┃─┃┃┃┃─┏┛┏┛─ "
+echo -e $g "┃┗━┛┃┏┛┗┛┃┃┃┃┃┃┃┃┗━┛┃┏┛┗┛┃┏┛━┗━┓ "
+echo -e $r "┗━━━┛┗━━━┛┗┛┗┛┗┛┗━━━┛┗━━━┛┗━━━━┛ "
+echo -e $r "                   TOOL BY UDMODZ"
+echo -e $b "      "
+echo -e $r "                     SUP එකක් දියම්"
+echo -e $b "      "
+echo -e $b "            https://www.youtube.com/@UDMODZ"
+echo -e $b "            https://whatsapp.com/channel/0029Va5e01M3LdQdtjYJjc3K"
+echo -e $b "            https://t.me/UDMODZ3"
+echo -e $b "            https://sl-ud-mods.yolasite.com"
+echo -e $b "            https://wa.me/94704638406"
+echo -e $b "            https://www.tiktok.com/@UDMODZ"
+echo
+echo
+echo
+echo
+
+echo -e $r "ENTER BANNER NAME THAT YOU WANT ? : \c"
+read varBname
+echo
+echo
+echo "figlet "$varBname" | lolcat " > Bname.txt
+echo "clear" > clear.txt
+echo "PS1='\$ ' " > Temp.txt
+
+
+
+rm -rf /data/data/com.termux/files/usr/etc/zshrc
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+
+
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+
+
+cat "clear.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "name.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Bname.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "Temp.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
+
+rm -rf Baname.txt clear.txt Temp.txt
+
+echo
+figlet FINISH | lolcat
+echo "                             THANKS FOR USING MY TOOL....😁" | lolcat
+echo""
+echo""
+echo""
+echo""
+echo -e $lb "                ▻▻▻▻ සබ්ස්කයිබ් කරපං....! ◅◅◅◅"
+echo""
+echo""
+echo""
+echo""
+  sleep 2
+  termux-open-url https://www.youtube.com/@UDMODZ
+
+echo -e $r "                       💻] TERMUX එක CLOSE කරල ආය RUN කරපං ⚘]          "
